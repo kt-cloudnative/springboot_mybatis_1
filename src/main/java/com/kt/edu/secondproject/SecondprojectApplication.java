6165fb39e75a31b25d90ca2e678d701e79827564
LEE SEOK HWAN <shclub@gmail.com>
2022-05-25 12:27:41 +0900

    second commit

--- src/main/java/com/kt/edu/secondproject/SecondprojectApplication.java
+++ src/main/java/com/kt/edu/secondproject/SecondprojectApplication.java
@@ -2,8 +2,7 @@ package com.kt.edu.secondproject;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@MapperScan(basePackages = "com.kt.edu.secondproject.repository")
